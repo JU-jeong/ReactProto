45f0def8ae98b228ba512fc692b75b0a70e479ca
wnwjdtjr77 <wnwjdtjr77@naver.com>
2021-11-13 15:24:07 +0900

    5강까지

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+ 
+const customers = [
+  {
+    'id' : 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '92020812',
+    'gender': '남자',
+    'job': '대학생',
+  },
+  {
+    'id' : 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길서',
+    'birthday': '92020812',
+    'gender': '남자',
+    'job': '대학생',
+  },
+  {
+    'id' : 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '홍길남',
+    'birthday': '92020812',
+    'gender': '남자',
+    'job': '대학생',
+  }
+]
+
+
 
 function App() {
   return (
-    <div className="gray-background">
-      <header className="App-header">
-        <img src={logo} lat="logo" alt="logo" />
-        <p>
-          최강에 이르는 자
-        </p>
-        <a>
-         
-        </a>
-      </header>
+    <div>
+      {
+        customers.map(c => {
+          return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+          );
+        })
+      }
     </div>
   );
 }
